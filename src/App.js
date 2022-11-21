ab84de806e246c0d3b68a5ad7e64a13f7e5515b6
hungpv <phanhung2702@gmail.com>
2022-11-21 14:32:14 +0700

    [Add] Views to router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
-import { Login } from '../src/views'
+import { Login, Heart } from '../src/views'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom'
+
+import { RequireAuth } from './components'
+import { AuthProvider } from './providers/use-auth'
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={(
+          <RequireAuth>
+            <Heart />
+          </RequireAuth>
+        )} />
+    </Routes>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <Login></Login>
-    </div>
+    <Router>
+      <div className='App'>
+        <AuthProvider>
+          <AppRoutes />
+        </AuthProvider>
+      </div>
+    </Router>
   );
 }
 
