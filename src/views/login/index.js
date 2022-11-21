834422424b327d4a867f01499f7f95fb9bf0b63c
hungpv <phanhung2702@gmail.com>
2022-11-21 14:38:41 +0700

    [Add] signin handler

--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,21 +1,45 @@
 import './index.css'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router'
+import { useAuth } from '../../providers/use-auth'
 
 export const Login = (props) => {
+  const { redirectTo = '/' } = props
+  const { attemptLogin } = useAuth()
+  const navigate = useNavigate()
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const signinHandler = async e => {
+    e.preventDefault()
+
+    setError('')
+
+    try {
+      await attemptLogin(username, password)
+
+      navigate(redirectTo, { replace: true })
+    } catch (error) {
+      setError(error.message)
+    }
+  }
+
   return (
     <div className='login-container'>
       <div className='login-form' >
         <div className='form-head'>Sign in</div>
         <div className='form-content'>
           <div className='input-container'>
-            <input className='username input' placeholder='Username' type="text" ></input>
-            <input className='password input' placeholder='Password' type="password" ></input>
+            <input className='username input' placeholder='Username' onChange={e => setUsername(e.target.value)} value={username} type="text" ></input>
+            <input className='password input' placeholder='Password' onChange={e => setPassword(e.target.value)} value={password} type="password" ></input>
             <div className='forgot-account-container'>
               <a className='forgot-account'>Forgot Username/Password?</a>
             </div>
           </div>
 
           <div className='signin-container'>
-            <button className='signin'>SIGN IN</button>
+            <button className='signin' onClick={signinHandler} >SIGN IN</button>
           </div>
 
           <div className='signup-container'>
