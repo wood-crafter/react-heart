75b44a3696385c1a3382773cfa7d2d1cbd5c9dab
hungpv <phanhung2702@gmail.com>
2022-11-21 17:03:39 +0700

    [Add] flipcard

--- src/views/heart/index.js
+++ src/views/heart/index.js
@@ -3,7 +3,16 @@ import './index.css'
 export const Heart = (props) => {
   return (
     <div className='heart-body'>
-      <div class="heart"></div>
+      <div className='card'>
+        <div className='content' >
+          <div className='front'>
+            some here
+          </div>
+          <div className='back'>
+            <div class="heart red"></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
