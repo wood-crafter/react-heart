dbb895913d628ef467c1d97d0b499b5e22182f6d
hungpv <phanhung2702@gmail.com>
2022-11-22 15:40:00 +0700

    [Draft] avatar for each

--- src/views/heart/index.js
+++ src/views/heart/index.js
@@ -1,6 +1,27 @@
+import { useContext } from 'react'
 import './index.css'
+import { AuthContext } from '../../providers/use-auth'
 
 export const Heart = (props) => {
+  const { user } = useContext(AuthContext)
+  let url = ''
+
+  switch (user.username) {
+    case 'aiinauu':
+      url = "../../../public/mee-avatar.jpg"
+      break
+    case 'vnanne':
+      url = "../../../public/vnanne-avatar.jpg"
+      break
+    case 'gogi':
+      url = "../../../public/gogi-avatar.jpg"
+      break
+  }
+
+  const myStyle = {
+    backgroundImage: `url(${url})`,
+  }
+
   return (
     <div className='heart-body'>
       <div className='card'>
@@ -8,7 +29,7 @@ export const Heart = (props) => {
           <div className='front'>
             <div className='person-head'></div>
             <div className='hexa-parent'>
-              <div className='person-avatar hexagon'></div>
+              <div className='person-avatar hexagon' style={myStyle}></div>
             </div>
             <div className='person-message'></div>
           </div>
