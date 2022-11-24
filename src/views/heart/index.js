0a243bcaa7271b92103f680d6f211ea361e58430
wood-crafter <phanhung2702@gmail.com>
2022-11-24 22:47:33 +0700

    [Add] person name

--- src/views/heart/index.js
+++ src/views/heart/index.js
@@ -5,16 +5,20 @@ import { AuthContext } from '../../providers/use-auth'
 export const Heart = (props) => {
   const { user } = useContext(AuthContext)
   let url = ''
+  let personName
 
   switch (user.username) {
     case 'aiinauu':
       url = process.env.REACT_APP_MEE_AVATAR_URL
+      personName = process.env.REACT_APP_MEE_TITLE
       break
     case 'vnanne':
       url = process.env.REACT_APP_VNANNE_AVATAR_URL
+      personName = process.env.REACT_APP_VNANNE_TITLE
       break
     case 'gogi':
       url = process.env.REACT_APP_GOGI_AVATAR_URL
+      personName = process.env.REACT_APP_GOGI_TITLE
       break
   }
 
@@ -27,7 +31,9 @@ export const Heart = (props) => {
       <div className='card'>
         <div className='content' >
           <div className='front'>
-            <div className='person-head'></div>
+            <div className='person-head'>
+              <div className='person-name'>{ personName }</div>
+            </div>
             <div className='hexa-parent'>
               <div className='person-avatar hexagon' style={myStyle}></div>
             </div>
